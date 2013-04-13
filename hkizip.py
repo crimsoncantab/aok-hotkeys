8de9c1febd58df740ba9d1ab22f80c12e37f4bde
Loren McGinnis <mcginnis.loren@gmail.com>
2013-04-13 11:55:58 -0600

    changed line endings, made hkizip a module

--- hkizip.py
+++ hkizip.py
@@ -1,1 +1,19 @@
-#!/bin/python3import sysimport zlibstdinb = sys.stdin.bufferstdoutb = sys.stdout.bufferinput = stdinb.read()if sys.argv[1] == 'd':	d = zlib.decompressobj(-zlib.MAX_WBITS)	output = d.decompress(input)	output += d.flush()else:	c = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, zlib.Z_DEFAULT_STRATEGY)	output = c.compress(input)	output += c.flush()stdoutb.write(output)
+#!/bin/python3
+import sys
+import zlib
+
+def compress(input):
+    c = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, zlib.Z_DEFAULT_STRATEGY)
+    output = c.compress(input)
+    return output + c.flush()
+
+def decompress(input):
+    d = zlib.decompressobj(-zlib.MAX_WBITS)
+    output = d.decompress(input)
+    return output + d.flush()
+
+if __name__ == '__main__':
+    stdinb = sys.stdin.buffer
+    stdoutb = sys.stdout.buffer
+    f = decompress if sys.argv[1] == 'd' else compress
+    stdoutb.write(f(stdinb.read()))
