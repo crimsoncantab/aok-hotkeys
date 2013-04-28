2ba7edcbddeae1f8c2c2b4743304bf33f0857bfb
Loren McGinnis <mcginnis.loren@gmail.com>
2013-04-27 18:33:32 -0600

    some testing for hkizip

--- modules/hkizip.py
+++ modules/hkizip.py
@@ -1,11 +1,20 @@
 #!/bin/python
 import sys
 import zlib
+import itertools
 
 def compress(input):
     c = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, zlib.Z_DEFAULT_STRATEGY)
     output = c.compress(input)
     return output + c.flush()
+    # comp, wbit, mem = 6, 12, 3
+    # c = zlib.compressobj(comp, zlib.DEFLATED, -wbit, mem, zlib.Z_DEFAULT_STRATEGY)
+    # for comp, wbit, mem, strat in itertools.product(range(-1, 10), range(8, 16), range(1,10), range(0,5)):
+        # c = zlib.compressobj(comp, zlib.DEFLATED, -wbit, mem, strat)
+        # output = c.compress(input) + c.flush()
+        # if len(output) == 597:
+            # with open('a%d_%d_%d_%d.hki' % (comp, wbit, mem, strat), 'wb') as out:
+                # out.write(output)
 
 def decompress(input):
     d = zlib.decompressobj(-zlib.MAX_WBITS)
@@ -21,3 +30,4 @@ if __name__ == '__main__':
         stdoutb = sys.stdout
     f = decompress if sys.argv[1] == 'd' else compress
     stdoutb.write(f(stdinb.read()))
+    # compress(sys.stdin.read())
