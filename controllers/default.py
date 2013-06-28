3894d8a9039ec29beaba3a8e4474aa75c329a473
Loren McGinnis <mcginnis.loren@gmail.com>
2013-06-27 23:26:51 -0600

    finished move controller functions

--- controllers/default.py
+++ controllers/default.py
@@ -14,28 +14,9 @@ def editor():
                 hk_desc = [(group, [(hk, hotkeys.hk_desc[hk]) for hk in hks]) for (group, hks) in hotkeys.hk_groups],
                 hk_versions = hotkeys.hk_versions, version = get_assign().version
                 ))
-    
-def version():
-    if request.vars.version not in [v[0] for v in hotkeys.hk_versions]:
-        raise HTTP(400, 'Bad version specified')
-    get_assign().version = request.vars.version
 
 def preset():
     redirect(URL('presets', 'get', r=request))
-
-def save():
-    update_assign(json.loads(request.vars.hotkeys))
-    return ''
-
-def player1():
-    response.headers['Content-Type'] = 'application/octet-stream'
-    assign = update_assign(json.loads(request.vars.hotkeys))
-    hkfile = load_file(assign.version)
-    for hotkey, value in assign.hotkeys.items():
-        if hotkey in hkfile:
-            hkfile[hotkey].update(value)
-    return hkfile.serialize()
-    
     
 def googled34aee2b940141cc():
     return open(os.path.join(request.folder, 'private', 'googled34aee2b940141cc.html')).read()
--- controllers/hotkeys.py
+++ controllers/hotkeys.py
@@ -1,7 +1,5 @@
 def get():
     assign = get_assign()
-    #from gluon.contrib import simplejson as json
-    #return json.dumps(assign.get_hotkeys(version_hotkeys(assign.version)))
     return assign.get_hotkeys(version_hotkeys(assign.version))
 
 def upload():
@@ -15,3 +13,21 @@ def upload():
     log.info('File version: {:s}'.format(hkfile.version))
     set_assign(hkfile)
     redirect(URL('default', 'editor'))
+
+def save():
+    update_assign(json.loads(request.vars.hotkeys))
+    return ''
+
+def download():
+    response.headers['Content-Type'] = 'application/octet-stream'
+    assign = update_assign(json.loads(request.vars.hotkeys))
+    hkfile = load_file(assign.version)
+    for hotkey, value in assign.hotkeys.items():
+        if hotkey in hkfile:
+            hkfile[hotkey].update(value)
+    return hkfile.serialize()
+
+def version():
+    if request.vars.version not in [v[0] for v in hotkeys.hk_versions]:
+        raise HTTP(400, 'Bad version specified')
+    get_assign().version = request.vars.version
