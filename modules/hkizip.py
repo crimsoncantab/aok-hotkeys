09193003922cf229daf05be1672e2364b66c66d2
Loren McGinnis <mcginnis.loren@gmail.com>
2019-11-24 13:41:12 -0700

    formatting, removing some comments

--- modules/hkizip.py
+++ modules/hkizip.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python
 import sys
 import zlib
-import itertools
+
 
 def compress(input):
-    c = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, zlib.Z_DEFAULT_STRATEGY)
+    c = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL,
+                         zlib.Z_DEFAULT_STRATEGY)
     output = c.compress(input)
     return output + c.flush()
-    # comp, wbit, mem = 6, 12, 3
-    # c = zlib.compressobj(comp, zlib.DEFLATED, -wbit, mem, zlib.Z_DEFAULT_STRATEGY)
-    # for comp, wbit, mem, strat in itertools.product(range(-1, 10), range(8, 16), range(1,10), range(0,5)):
-        # c = zlib.compressobj(comp, zlib.DEFLATED, -wbit, mem, strat)
-        # output = c.compress(input) + c.flush()
-        # if len(output) == 597:
-            # with open('a%d_%d_%d_%d.hki' % (comp, wbit, mem, strat), 'wb') as out:
-                # out.write(output)
+
 
 def decompress(input):
     d = zlib.decompressobj(-zlib.MAX_WBITS)
     output = d.decompress(input)
     return output + d.flush()
+
 
 if __name__ == '__main__':
     try:
